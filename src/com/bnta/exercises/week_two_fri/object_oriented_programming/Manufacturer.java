66145c989e8a9d9bb702e0d2a0dde967d15134b4
Marcy Calderon <marcycalderon@hotmail.co.uk>
2022-02-02 00:31:41 +0000

    created enum

---
+++ src/com/bnta/exercises/week_two_fri/object_oriented_programming/Manufacturer.java
@@ -0,0 +1,11 @@
+package com.bnta.exercises.week_two_fri.object_oriented_programming;
+
+public enum Manufacturer {
+    TOYOTA,
+    MERCEDES,
+    LAMBORGHINI,
+    MASERATI,
+    BMW,
+    PORSCHE
+
+}
